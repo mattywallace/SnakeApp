b46b923d2d1eca029dbd6f3904651d80968866e9
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-04-30 17:16:16 -0500

    required and tested jsonify, passes test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, jsonify
 
 DEBUG=True
 PORT=8000
@@ -6,8 +6,12 @@ PORT=8000
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def test_connect():
 	return 'Connection Test'
+
+@app.route('/test_json')
+def test_json():
+	return jsonify(['Matthew', 'Mark', 'Luke', 'John'])
 
 if __name__ =='__main__':
 	app.run(debug=DEBUG, port=PORT)
