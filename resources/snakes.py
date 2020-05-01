d63fdb19e8b60f98452a9cb6e7bfdf6ab5f47147
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-05-01 10:35:23 -0500

    the payload is reading in the terminal, arabian cobra added

--- resources/snakes.py
+++ resources/snakes.py
@@ -13,24 +13,15 @@ def create_snake():
 	payload = request.get_json()
 	print(payload)
 	new_snake = models.Snake.create(
-		species=payload['species'],
-		family=payload['family'],
-		habitat=payload['habitat'],
-		average_size=payload['average_size'],
-		picture=payload['picture'],
-		description=payload['description'],
-		added_by=payload['added_by']
-		)
+					species=payload['species'],
+					family=payload['family'],
+					habitat=payload['habitat'],
+					venomous=payload['venomous'],
+					average_size=payload['average_size'],
+					picture=payload['picture'],
+					description=payload['description'],
+					added_by=payload['added_by']
+				)
 	return " We have hit the snake create route -- check terminal"
 
 
-
-	class Snake(Model):
-	species = CharField() 
-	habitat = CharField()
-	family = CharField()
-	average_size = IntegerField()
-	venomous = BooleanField()
-	description = TextField()
-	picture = CharField()
-	added_by = CharField()
