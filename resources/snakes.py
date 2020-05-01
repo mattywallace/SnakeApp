a33ffb6c8fd8752719d7afa6fdd3cb1b3e97e307
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-04-30 21:04:41 -0500

    wrote out out new snake in resource create route

--- resources/snakes.py
+++ resources/snakes.py
@@ -12,4 +12,25 @@ def snakes_index():
 def create_snake():
 	payload = request.get_json()
 	print(payload)
+	new_snake = models.Snake.create(
+		species=payload['species'],
+		family=payload['family'],
+		habitat=payload['habitat'],
+		average_size=payload['average_size'],
+		picture=payload['picture'],
+		description=payload['description'],
+		added_by=payload['added_by']
+		)
 	return " We have hit the snake create route -- check terminal"
+
+
+
+	class Snake(Model):
+	species = CharField() 
+	habitat = CharField()
+	family = CharField()
+	average_size = IntegerField()
+	venomous = BooleanField()
+	description = TextField()
+	picture = CharField()
+	added_by = CharField()
