69094a7624971870cd58c35b34172ca5819a0c96
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-05-01 11:30:50 -0500

    added try catch to create route to stop duplication in database

--- resources/snakes.py
+++ resources/snakes.py
@@ -8,30 +8,40 @@ snakes = Blueprint('snakes', 'snakes')
 
 @snakes.route('/', methods=['GET'])
 def snakes_index():
-	return 'snakes resource and index route hitting'
+	result = models.Snake.select()
+	print('')
+	print('result of snake select query')
+	print(result)
+	snake_dicts= [model_to_dict(snake) for snake in result]
+	print('this is snake dicts')
+	print(snake_dicts)
+	return jsonify({
+		'data': snake_dicts,
+		'message': f" Successfully found {len(snake_dicts)} snakes",
+		'status': 200
+	}) 
+
 
 @snakes.route('/', methods=['POST'])
 def create_snake():
 	payload = request.get_json()
 	print(payload)
-	new_snake = models.Snake.create(
-					species=payload['species'],
-					family=payload['family'],
-					habitat=payload['habitat'],
-					venomous=payload['venomous'],
-					average_size=payload['average_size'],
-					picture=payload['picture'],
-					description=payload['description'],
-					added_by=payload['added_by']
-				)
-	print(new_snake)
-	print(new_snake.__dict__)
-	print(dir(new_snake))
-	snake_dict = model_to_dict(new_snake)
-	return jsonify(
-		data=snake_dict,
-		message='You have created a snake',
-		status=201
-	), 201
+	try:
+		snake = models.Snake.get(models.Snake.species == payload['species'])
+		print('this is the snake on try')
+		print(snake)
+		return print('this snake is already created')
+	except models.DoesNotExist:
+		new_snake = models.Snake.create(**payload)
+		print('this is the snake on except')
+		print(new_snake)
+		print(new_snake.__dict__)
+		print(dir(new_snake))
+		snake_dict = model_to_dict(new_snake)
+		return jsonify(
+			data=snake_dict,
+			message='You have created a snake',
+			status=201
+			), 201
 
 
