6823137bc25aa16973eb1979096461fcd1b78c35
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-05-04 09:40:12 -0500

    user can only update and add files that are there own

--- app.py
+++ app.py
@@ -30,6 +30,16 @@ def load_user(user_id):
 	except models.DoesNotExist:
 		return None
 
+@login_manager.unauthorized_handler
+def unauthorized():
+	return jsonify(
+		data={
+		"error: user not logged in"
+		},
+		message="you must be logged in to access snakes",
+		status=401
+	), 401
+
 CORS(snakes, origins=['http://localhost:3000'], supports_credentials=True)
 CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
 
--- resources/snakes.py
+++ resources/snakes.py
@@ -4,12 +4,15 @@ from flask import Blueprint, request, jsonify
 
 from playhouse.shortcuts import model_to_dict
 
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 snakes = Blueprint('snakes', 'snakes')
 
 @snakes.route('/', methods=['GET'])
+@login_required
 def snakes_index():
+	""" Shows all the snakes that belong to the current user """
+	
 	current_user_snake_dicts = [model_to_dict(snake) for snake in current_user.snakes]
 	
 	for snake_dict in current_user_snake_dicts:
@@ -24,6 +27,7 @@ def snakes_index():
 
 
 @snakes.route('/', methods=['POST'])
+@login_required
 def create_snake():
 	payload = request.get_json()
 	print(payload)
@@ -56,35 +60,96 @@ def create_snake():
 			status=201
 			), 201
 
+
+
+
+@snakes.route('/<id>', methods=['GET'])
+def show_snake(id):
+	snake = models.Snake.get_by_id(id)
+	if not current_user.is_authenticated:
+		return jsonify(
+			data={
+			'species': snake.species,
+			'picture': snake.picture, 
+			},
+			message='Login or register to learn more about each snake',
+			status=200
+		), 200
+	else:
+		snake_dict = model_to_dict(snake)
+		snake_dict['added_by'].pop('password')
+		return jsonify(
+			data=snake_dict,
+			message=f"Found dog with id {id}",
+			status=200
+		), 200
+	
+
+
+
+
+
+
 @snakes.route('/<id>', methods=['DELETE'])
+@login_required
 def delete_snake(id):
-	delete_query = models.Snake.delete().where(models.Snake.id == id)
-	num_of_rows_deleted = delete_query.execute()
-	print(num_of_rows_deleted)
-	return jsonify(
-		data={},
-		message="Successfully deleted {} snake with id {}".format(num_of_rows_deleted, id),
-		status=200,
-	), 200
+	try:
+		snake_to_delete = models.Snake.get_by_id(id)
+		if snake_to_delete.added_by.id == current_user.id:
+			return jsonify(
+				data={},
+				message=f"Successfully deleted snake with id {id}",
+				status=200
+			), 200 
+		else:
+			return jsonify(
+				data={
+				'error': '403 forbidden'
+				},
+				message='This snake was not added by you. Only the creator of this article can remove it.',
+				status=403
+			), 403
+	except models.DoesNotExist:
+		return jsonify(
+		      data={
+		        'error': '404 Not found'
+		      },
+		      message="There is no dog with that ID.",
+		      status=404
+		    ), 404
+
 
 
 
 
 @snakes.route('/<id>', methods=['PUT'])
+@login_required
 def update_snake(id):
 	payload = request.get_json()
-	update_query = models.Snake.update( 
-		description=payload['description'],
-		picture=payload['picture']
-	).where(models.Snake.id == id)
-	num_of_rows_modified = update_query.execute()
-	updated_snake = models.Snake.get_by_id(id)
-	updated_snake_dict = model_to_dict(updated_snake)
-	return jsonify(
-		data=updated_snake_dict, 
-		message=f"Successsfully updated snake with id {id}",
-		status=200
-	), 200
+	snake_to_update = models.Snake.get_by_id(id)
+	if snake_to_update.added_by.id == current_user.id:
+		if 'description' in payload:
+			snake_to_update.descrption = payload['description']
+		if 'picture' in payload:
+			snake_to_update.picture = payload['picture']
+		snake_to_update.save()
+		updated_snake_dict = model_to_dict(snake_to_update)
+		updated_snake_dict['added_by'].pop('password')
+		return jsonify(
+        	data=updated_snake_dict,
+        	message=f"Successfully updated snake with id {id}",
+        	status=200
+      	), 200
+	else:
+		return jsonify(
+			data={
+			'error':'403 Forbidden'
+			},
+			message="Only the user who created the article can update the snake",
+			status=403
+		), 403 
+
+      
 
 
 
